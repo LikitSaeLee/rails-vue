f1b382df105fdcf92bd8b404b8db376e5a9974bd
Likit Sae Lee <likit.usm@gmail.com>
2018-07-09 16:25:19 +0800

    generated entry page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'apps#index'
+
+  get 'apps/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
